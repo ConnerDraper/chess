9acee9e571f4e6758abb0d9b828ae860bc91ee3c
Conner <draper.conner@gmail.com>
2026-09-20 00:58:40 +0000

    Implemented pawn forward logic; implemented rook logic entirely.

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -11,7 +11,7 @@ import java.util.Collection;
 public class ChessGame {
     
     private ChessBoard board;
-    private Teamcolor teamTurn;
+    private TeamColor teamTurn;
 
     public ChessGame() {
         this.board = new ChessBoard();
--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,8 @@
 package chess;
+import chess.ChessGame.TeamColor;
 
 import java.util.Collection;
+import java.util.ArrayList;
 
 /**
  * Represents a single chess piece
@@ -12,7 +14,6 @@ public class ChessPiece {
 
     private final ChessGame.TeamColor pieceColor;
     private ChessPiece.PieceType type;
-    private ChessPosition position;
 
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
         this.pieceColor = pieceColor;
@@ -53,32 +54,132 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
-    }
+        Collection<ChessMove> moves = new ArrayList<>();
 
-    /**
-     * Gives potential moves for a given piece.
-     * Does not account for occupied squares or game state.
-     * Only returns squares that a piece could physically move to.
-     * Helper function for pieceMoves.
-     * @param board
-     * @param myPosition
-     * @return
-     */
-    public Collection<ChessMove> potentialPieceMoves(ChessBoard board, ChessPosition myPosition) {
-        int[][] moves = new int[8][8];
         TeamColor otherColor;
-        if (this.pieceColor == teamColor.WHITE) otherColor = teamColor.BLACK;
-        else if (this.pieceColor == teamColor.BLACK) otherColor = teamColor.WHITE;
+        if (this.pieceColor == TeamColor.WHITE) otherColor = TeamColor.BLACK;
+        else if (this.pieceColor == TeamColor.BLACK) otherColor = TeamColor.WHITE;
         else throw new RuntimeException("Error: no pieceColor assigned to piece!");
-
         throw new RuntimeException("Still need to finish this method (ChessPiece potentialPieceMoves");
 
+        int row = myPosition.getRow();
+        int column = myPosition.getColumn();
+
         if (this.type == PieceType.PAWN) {
-            
+            int forward;
+            int promotionRow;
+            if (this.pieceColor == TeamColor.WHITE) {
+                forward = 1;
+                promotionRow = 8;
+            }
+            else {
+                forward = -1;
+                promotionRow = 1;
+            }
+
+            // it is impossible for a pawn to be on the edge - so we don't check for it.
+            // if forward space is available...
+            if (board.getPiece(row, column + forward) == null) {
+                ChessPosition forwardSpace = ChessPosition(row, column + forward);
+                // if forward space is a promotion...
+                if (forwardSpace == promotionRow) {
+                    for (ChessPiece p : PieceType.values()) {
+                        if (p != PieceType.PAWN) {
+                            ChessMove m = new ChessMove(myPosition, forwardSpace, p);
+                            moves.add(m);
+                        }
+                    }
+
+                }
+                // if forward space is NOT a promotion...
+                else {
+                    ChessMove m = new ChessMove(myPosition, forwardSpace);
+                    moves.add(m);
+                }
+            }
+
+
         }
         else if (this.type == PieceType.ROOK) {
+            // positive direction of row
+            for (int i = row; i <= 8; i++) {
+                if (board.getPiece(i, column) != null) {
+                    ChessPosition newPosition = ChessPosition(i, column);
+                    ChessMove m = new ChessMove(myPosition, newPosition);
+                    moves.add(m);
+                }
+                else {
+                    if (board.getPiece(i, column).getTeamColor() == otherColor) {
+                        ChessPosition newPosition = ChessPosition(i, column);
+                        ChessMove m = new ChessMove(myPosition, newPosition);
+                        moves.add(m);
+                    }
+                    break;
+                }
+            }
 
+            // negative direction of row
+            for (int i = row; i >= 1; i--) {
+                if (board.getPiece(i, column) != null) {
+                    ChessPosition newPosition = ChessPosition(i, column);
+                    ChessMove m = new ChessMove(myPosition, newPosition);
+                    moves.add(m);
+                }
+                else {
+                    if (board.getPiece(i, column).getTeamColor() == otherColor) {
+                        ChessPosition newPosition = ChessPosition(i, column);
+                        ChessMove m = new ChessMove(myPosition, newPosition);
+                        moves.add(m);
+                    }
+                    break;
+                }
+            }
+
+            // positive direction of column
+            for (int i = column; i <= 8; i++) {
+                if (board.getPiece(row, i) != null) {
+                    ChessPosition newPosition = ChessPosition(row, i);
+                    ChessMove m = new ChessMove(myPosition, newPosition);
+                    moves.add(m);
+                }
+                else {
+                    if (board.getPiece(row, i).getTeamColor() == otherColor) {
+                        ChessPosition newPosition = ChessPosition(row, i);
+                        ChessMove m = new ChessMove(myPosition, newPosition);
+                        moves.add(m);
+                    }
+                    break;
+                }
+            }
+
+            // negative direction of column
+            for (int i = column; i >= 1; i--) {
+                if (board.getPiece(row, i) != null) {
+                    ChessPosition newPosition = ChessPosition(row, i);
+                    ChessMove m = new ChessMove(myPosition, newPosition);
+                    moves.add(m);
+                }
+                else {
+                    if (board.getPiece(row, i).getTeamColor() == otherColor) {
+                        ChessPosition newPosition = ChessPosition(row, i);
+                        ChessMove m = new ChessMove(myPosition, newPosition);
+                        moves.add(m);
+                    }
+                    break;
+                }
+            }
+        }
+        else if (this.type == PieceType.KNIGHT) {
+            throw new RuntimeException("Error: Not yet implemented.");
+        }
+        else if (this.type == PieceType.BISHOP) {
+            throw new RuntimeException("Error: Not yet implemented.");
+        }
+        else if (this.type == PieceType.QUEEN) {
+            throw new RuntimeException("Error: Not yet implemented.");
+        }
+        else if (this.type == PieceType.KING) {
+            throw new RuntimeException("Error: Not yet implemented.");
         }
     }
 }
